33d5226756367f1bcd655bafef848e5544742f0b
Benito Palacios Sánchez <benito356@gmail.com>
2014-12-28 17:32:22 +0100

    Skill long name in train menu

--- Assembly/overlay9_17.asm
+++ Assembly/overlay9_17.asm
@@ -24,6 +24,7 @@
 .include fileformats\spellsname\spells_ov17.asm
 .include fileformats\familiarname\familiar_main.asm
 .include fileformats\skillsname\skills_main.asm
+.include fileformats\skillsname\skills_train.asm
 .include fileformats\skills_spells_getName.asm
 .include textbox\cutscenes.asm
 .include textbox\script_dialog.asm
