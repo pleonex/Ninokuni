31c802b8b5b76ffbecff5f177dfac836eb994ac6
Benito Palacios Sánchez <benito356@gmail.com>
2015-02-04 19:44:30 +0100

    Move numbers in check sheet menu

--- Assembly/textbox/menu_checksheet.asm
+++ Assembly/textbox/menu_checksheet.asm
@@ -19,3 +19,47 @@
 .arm
 .org 0x02095DFC
   MOV r1, #0        ; Originally 0x3
+
+
+;; Position of numbers
+.arm
+.org 0x02095E64
+  MOV R8, #0xC1     ; Common constant, used for thrid column number X pos
+.org 0x02095E7C
+  MOV R6, #0x3F     ; Common constant, used for second text line Y pos
+
+;; Player name
+.org 0x02095E50
+  MOV R1, #0x76     ; X pos, original 0x76
+  MOV R2, #0x17     ; Y pos, original 0x17
+
+;; Number of familiar catched
+.org 0x02095E6C
+  MOV R1, R8        ; X pos, original 0xC1 (r8)
+  MOV R3, R7
+  MOV R2, #0x2E     ; Y pos, original 0x2E
+
+;; Number of battles
+.org 0x02095E84
+  MOV r1, #0x82     ; X pos, original 0x63
+  MOV r2, #0x40     ; Y pos, original 0x3F (r6)
+
+;; Battles won
+.org 0x02095E98
+  MOV r1, #0xB1     ; X pos, original 0x63
+  MOV r2, #0x40     ; Y pos, original 0x3F (r6)
+
+;; Battles lose
+.org 0x02095EAC
+  MOV r1, #0xE0     ; X pos, original 0xC1 (r8)
+  MOV r2, #0x40     ; Y pos, original 0x3F (r6)
+
+;; Victory ratio
+.org 0x02095EC0
+  MOV R1, R8        ; X pos, original 0xC1 (r8)
+  MOV R2, #0x52     ; Y pos, original 0x52
+
+;; Number of exchanged familiars
+.org 0x02095ED0
+  MOV R1, R8        ; X pos, original 0xC1 (r8)
+  MOV R2, R6        ; Y pos, original 0x3F (r6)
