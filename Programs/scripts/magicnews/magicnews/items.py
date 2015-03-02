62f37a81a5972fffd0514670af6b4157697f549a
Benito Palacios Sánchez <benito356@gmail.com>
2015-03-02 20:59:19 +0100

    Magicnews: Created magicnew item

--- Programs/scripts/magicnews/magicnews/items.py
+++ Programs/scripts/magicnews/magicnews/items.py
@@ -1,14 +1,25 @@
 # -*- coding: utf-8 -*-
-
-# Define here the models for your scraped items
-#
-# See documentation in:
-# http://doc.scrapy.org/en/latest/topics/items.html
+###############################################################################
+#  Connect to game-kouryaku.info to download all the magic news - V1.0        #
+#  Copyright 2015 Benito Palacios (aka pleonex)                               #
+#                                                                             #
+#  Licensed under the Apache License, Version 2.0 (the "License");            #
+#  you may not use this file except in compliance with the License.           #
+#  You may obtain a copy of the License at                                    #
+#                                                                             #
+#      http://www.apache.org/licenses/LICENSE-2.0                             #
+#                                                                             #
+#  Unless required by applicable law or agreed to in writing, software        #
+#  distributed under the License is distributed on an "AS IS" BASIS,          #
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   #
+#  See the License for the specific language governing permissions and        #
+#  limitations under the License.                                             #
+###############################################################################
 
 import scrapy
 
 
 class MagicnewsItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    title = scrapy.Field()
+    date = scrapy.Field()
+    body = scrapy.Field()
