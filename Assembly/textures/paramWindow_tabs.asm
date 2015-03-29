a439f84477ec7e1f4a4500aa7e5688d487c6e793
Benito Palacios Sánchez <benito356@gmail.com>
2015-03-29 10:49:18 +0200

    Fix crash due to wrong code address in paramwindow

--- Assembly/textures/paramWindow_tabs.asm
+++ Assembly/textures/paramWindow_tabs.asm
@@ -14,14 +14,14 @@
 ;   + SP+18: Height
 ;   + SP+1C: Palette index
 
-@Ystart  equ -0x20
-@Xend    equ -0x1C
-@Yend    equ -0x18
-@Xout    equ -0x14
-@Yout    equ -0x10
-@Width   equ -0x0C
-@Height  equ -0x08
-@Palette equ -0x04
+@Ystart  equ 0x00
+@Xend    equ 0x04
+@Yend    equ 0x08
+@Xout    equ 0x0C
+@Yout    equ 0x10
+@Width   equ 0x14
+@Height  equ 0x18
+@Palette equ 0x1C
 @v3d_setSubImage equ 0x020FCD7C
 
 .arm
@@ -37,7 +37,7 @@
   MOV     R7, #77
   MOV     R11, #79
 
-TabLoop:
+@TabLoop:
   MOV     R2, R9,LSR#31
   MOV     R0, R9,ASR#1              ; For palette (1)
   RSB     R1, R2, R9,LSL#31
@@ -46,31 +46,31 @@ TabLoop:
   AND     R0, R0, #0xFF             ; (1) ...
   CMP     R1, #1
   MOV     R2, #0
-  BNE     TabNonSelected
-  STR     R8, [SP,#0x20+@Ystart]
+  BNE     @TabNonSelected
+  STR     R8, [SP,@Ystart]
   STMFA   SP, {R7,R11}
-  STR     R6, [SP,#0x20+@Xout]
-  STR     R5, [SP,#0x20+@Yout]
-  STR     R7, [SP,#0x20+@Width]
+  STR     R6, [SP,@Xout]
+  STR     R5, [SP,@Yout]
+  STR     R7, [SP,@Width]
   MOV     R1, #21
-  B       SetTab
+  B       @SetTab
 
-TabNonSelected:
+@TabNonSelected:
   MOV     R1, #81
-  STR     R1, [SP,#0x20+@Ystart]
+  STR     R1, [SP,@Ystart]
   MOV     R1, #77
-  STR     R1, [SP,#0x20+@Xend]
+  STR     R1, [SP,@Xend]
   MOV     R1, #101
-  STR     R1, [SP,#0x20+@Yend]
-  STR     R6, [SP,#0x20+@Xout]
-  STR     R4, [SP,#0x20+@Yout]
+  STR     R1, [SP,@Yend]
+  STR     R6, [SP,@Xout]
+  STR     R4, [SP,@Yout]
   MOV     R1, #77
-  STR     R1, [SP,#0x20+@Width]
+  STR     R1, [SP,@Width]
   MOV     R1, #20
 
-SetTab:
-  STR     R1, [SP,#0x20+@Height]
-  STR     R0, [SP,#0x20+@Palette]
+@SetTab:
+  STR     R1, [SP,@Height]
+  STR     R0, [SP,@Palette]
   LDR     R0, [R10,#0x5C]
   MOV     R1, R9
   ADD     R0, R0, #0x1A8
@@ -79,25 +79,26 @@ SetTab:
   ADD     R0, R9, #1
   AND     R9, R0, #0xFF
   CMP     R9, #8
-  BCC     TabLoop
+  BCC     @TabLoop
 
 ; # HP image (0/0): [77, 19] -> [91, 28]
+.org 0x020A7118
   MOV     R9, #28
   MOV     R6, #19
   MOV     R1, #0
-  STR     R6, [SP,#0x20+@Ystart]
+  STR     R6, [SP,@Ystart]
   MOV     R0, #91
   STMFA   SP, {R0,R9}
   SUB     R8, R9, #0x22
-  STR     R8, [SP,#0x20+@Xout]
+  STR     R8, [SP,@Xout]
   SUB     R5, R9, #0x20
-  STR     R5, [SP,#0x20+@Yout]
+  STR     R5, [SP,@Yout]
   MOV     R4, #14
-  STR     R4, [SP,#0x20+@Width]
+  STR     R4, [SP,@Width]
   MOV     R0, #9
-  STR     R0, [SP,#0x20+@Height]
+  STR     R0, [SP,@Height]
   MOV     R0, #7
-  STR     R0, [SP,#0x20+@Palette]
+  STR     R0, [SP,@Palette]
   LDR     R0, [R10,#0x60]
   MOV     R2, R1
   ADD     R0, R0, #0x128
@@ -106,16 +107,16 @@ SetTab:
 
 ; # MP image (0/0): [92, 19] -> [106, 28]
   MOV     R1, #0
-  STR     R6, [SP,#0x20+@Ystart]
+  STR     R6, [SP,@Ystart]
   MOV     R0, #106
   STMFA   SP, {R0,R9}
-  STR     R8, [SP,#0x20+@Xout]
-  STR     R5, [SP,#0x20+@Yout]
-  STR     R4, [SP,#0x20+@Width]
+  STR     R8, [SP,@Xout]
+  STR     R5, [SP,@Yout]
+  STR     R4, [SP,@Width]
   MOV     R0, #9
-  STR     R0, [SP,#0x20+@Height]
+  STR     R0, [SP,@Height]
   MOV     R0, #7
-  STR     R0, [SP,#0x20+@Palette]
+  STR     R0, [SP,@Palette]
   LDR     R0, [R10,#0x64]
   MOV     R2, R1
   ADD     R0, R0, #0x128
@@ -123,19 +124,20 @@ SetTab:
   BL      @v3d_setSubImage
 
 ; # Sword icon (0/0): [0, 0] -> [16, 16]
+.org 0x020A71D0
   MOV     R1, #0
   MOV     R0, #0
-  STR     R0, [SP,#0x20+@Ystart]
+  STR     R0, [SP,@Ystart]
   MOV     R6, #16
-  STR     R6, [SP,#0x20+@Xend]
-  STR     R6, [SP,#0x20+@Yend]
+  STR     R6, [SP,@Xend]
+  STR     R6, [SP,@Yend]
   SUB     R5, R6, #0x18
-  STR     R5, [SP,#0x20+@Xout]
-  STR     R5, [SP,#0x20+@Yout]
-  STR     R6, [SP,#0x20+@Width]
-  STR     R6, [SP,#0x20+@Height]
+  STR     R5, [SP,@Xout]
+  STR     R5, [SP,@Yout]
+  STR     R6, [SP,@Width]
+  STR     R6, [SP,@Height]
   MOV     R0, #5
-  STR     R0, [SP,#0x20+@Palette]
+  STR     R0, [SP,@Palette]
   LDR     R0, [R10,#0x68]
   MOV     R2, R1
   ADD     R0, R0, #0x128
@@ -144,15 +146,15 @@ SetTab:
 
 ; # Shield icon (1/0): [16, 0] -> [32, 16]
   MOV     R0, #0
-  STR     R0, [SP,#0x20+@Ystart]
+  STR     R0, [SP,@Ystart]
   MOV     R4, #32
   STMFA   SP, {R4,R6}
-  STR     R5, [SP,#0x20+@Xout]
-  STR     R5, [SP,#0x20+@Yout]
-  STR     R6, [SP,#0x20+@Width]
-  STR     R6, [SP,#0x20+@Height]
+  STR     R5, [SP,@Xout]
+  STR     R5, [SP,@Yout]
+  STR     R6, [SP,@Width]
+  STR     R6, [SP,@Height]
   MOV     R0, #5
-  STR     R0, [SP,#0x20+@Palette]
+  STR     R0, [SP,@Palette]
   LDR     R0, [R10,#0x68]
   MOV     R1, #1
   ADD     R0, R0, #0x128
@@ -163,15 +165,15 @@ SetTab:
 ; # Wand icon (2/0): [32, 0] -> [48, 16]
   MOV     R0, #0
   MOV     R3, R4
-  STR     R0, [SP,#0x20+@Ystart]
+  STR     R0, [SP,@Ystart]
   MOV     R4, #48
   STMFA   SP, {R4,R6}
-  STR     R5, [SP,#0x20+@Xout]
-  STR     R5, [SP,#0x20+@Yout]
-  STR     R6, [SP,#0x20+@Width]
-  STR     R6, [SP,#0x20+@Height]
+  STR     R5, [SP,@Xout]
+  STR     R5, [SP,@Yout]
+  STR     R6, [SP,@Width]
+  STR     R6, [SP,@Height]
   MOV     R0, #6
-  STR     R0, [SP,#0x20+@Palette]
+  STR     R0, [SP,@Palette]
   LDR     R0, [R10,#0x68]
   MOV     R1, #2
   ADD     R0, R0, #0x128
@@ -180,15 +182,15 @@ SetTab:
 
 ; # Bag icon (3/0): [48, 0] -> [64, 16]
   MOV     R0, #0
-  STR     R0, [SP,#0x20+@Ystart]
+  STR     R0, [SP,@Ystart]
   MOV     R0, #64
   STMFA   SP, {R0,R6}
-  STR     R5, [SP,#0x20+@Xout]
-  STR     R5, [SP,#0x20+@Yout]
-  STR     R6, [SP,#0x20+@Width]
-  STR     R6, [SP,#0x20+@Height]
+  STR     R5, [SP,@Xout]
+  STR     R5, [SP,@Yout]
+  STR     R6, [SP,@Width]
+  STR     R6, [SP,@Height]
   MOV     R0, #6
-  STR     R0, [SP,#0x20+@Palette]
+  STR     R0, [SP,@Palette]
   LDR     R0, [R10,#0x68]
   MOV     R3, R4
   ADD     R0, R0, #0x128
--- Assembly/textures/systemImage_smallArrows.asm
+++ Assembly/textures/systemImage_smallArrows.asm
@@ -1,4 +1,5 @@
 ;;  Modify the subimages position of the texture battles images.
+;;  This continues paramWindow_tabs
 ;
 ; Arguments of v3d_setSubImage function:
 ;   + R0: Sprite struct
@@ -14,14 +15,14 @@
 ;   + SP+18: Height
 ;   + SP+1C: Palette index
 
-@Ystart  equ -0x20
-@Xend    equ -0x1C
-@Yend    equ -0x18
-@Xout    equ -0x14
-@Yout    equ -0x10
-@Width   equ -0x0C
-@Height  equ -0x08
-@Palette equ -0x04
+@Ystart  equ 0x00
+@Xend    equ 0x04
+@Yend    equ 0x08
+@Xout    equ 0x0C
+@Yout    equ 0x10
+@Width   equ 0x14
+@Height  equ 0x18
+@Palette equ 0x1C
 @v3d_setSubImage equ 0x020FCD7C
 
 .arm
@@ -31,20 +32,21 @@
   MOV     R7, #10
 
 ; # Small Up Arrow (0/0): [29, 28] -> [39, 40]
-  STR     R9, [SP,#0x20+@Ystart]
+.org 0x020A7318
+  STR     R9, [SP,@Ystart]
   MOV     R11, #39
   MOV     R6, #40
-  STR     R11, [SP,#0x20+@Xend]
-  STR     R6, [SP,#0x20+@Yend]
+  STR     R11, [SP,@Xend]
+  STR     R6, [SP,@Yend]
   SUB     R5, R6, #0x2D
-  STR     R5, [SP,#0x20+@Xout]
-  STR     R8, [SP,#0x20+@Yout]
+  STR     R5, [SP,@Xout]
+  STR     R8, [SP,@Yout]
   MOV     R1, #0
-  STR     R7, [SP,#0x20+@Width]
+  STR     R7, [SP,@Width]
   MOV     R4, #12
-  STR     R4, [SP,#0x20+@Height]
+  STR     R4, [SP,@Height]
   MOV     R0, #1
-  STR     R0, [SP,#0x20+@Palette]
+  STR     R0, [SP,@Palette]
   LDR     R0, [R10,#0x6C]
   MOV     R2, R1
   ADD     R0, R0, #0x128
@@ -53,13 +55,13 @@
 
 ; # Small Up Arrow Other Palette (1/0): [29, 28] -> [39, 40]
   STMEA   SP, {R9,R11}
-  STR     R6, [SP,#0x20+@Yend]
-  STR     R5, [SP,#0x20+@Xout]
-  STR     R8, [SP,#0x20+@Yout]
-  STR     R7, [SP,#0x20+@Width]
-  STR     R4, [SP,#0x20+@Height]
+  STR     R6, [SP,@Yend]
+  STR     R5, [SP,@Xout]
+  STR     R8, [SP,@Yout]
+  STR     R7, [SP,@Width]
+  STR     R4, [SP,@Height]
   MOV     R0, #2
-  STR     R0, [SP,#0x20+@Palette]
+  STR     R0, [SP,@Palette]
   LDR     R0, [R10,#0x6C]
   MOV     R1, #1
   ADD     R0, R0, #0x128
@@ -69,13 +71,13 @@
 
 ; # Small Up Arrow Other Palette (2/0): [29, 28] -> [39, 40]
   STMEA   SP, {R9,R11}
-  STR     R6, [SP,#0x20+@Yend]
-  STR     R5, [SP,#0x20+@Xout]
-  STR     R8, [SP,#0x20+@Yout]
-  STR     R7, [SP,#0x20+@Width]
-  STR     R4, [SP,#0x20+@Height]
+  STR     R6, [SP,@Yend]
+  STR     R5, [SP,@Xout]
+  STR     R8, [SP,@Yout]
+  STR     R7, [SP,@Width]
+  STR     R4, [SP,@Height]
   MOV     R0, #3
-  STR     R0, [SP,#0x20+@Palette]
+  STR     R0, [SP,@Palette]
   LDR     R0, [R10,#0x6C]
   MOV     R1, #2
   ADD     R0, R0, #0x128
@@ -85,13 +87,13 @@
 
 ; # Small Up Arrow Other Palette (3/0): [29, 28] -> [39, 40]
   STMEA   SP, {R9,R11}
-  STR     R6, [SP,#0x20+@Yend]
-  STR     R5, [SP,#0x20+@Xout]
-  STR     R8, [SP,#0x20+@Yout]
-  STR     R7, [SP,#0x20+@Width]
-  STR     R4, [SP,#0x20+@Height]
+  STR     R6, [SP,@Yend]
+  STR     R5, [SP,@Xout]
+  STR     R8, [SP,@Yout]
+  STR     R7, [SP,@Width]
+  STR     R4, [SP,@Height]
   MOV     R0, #4
-  STR     R0, [SP,#0x20+@Palette]
+  STR     R0, [SP,@Palette]
   LDR     R0, [R10,#0x6C]
   MOV     R1, #3
   ADD     R0, R0, #0x128
@@ -101,13 +103,13 @@
 
 ; # Small Up Arrow Other Palette (4/0): [29, 28] -> [39, 40]
   STMEA   SP, {R9,R11}
-  STR     R6, [SP,#0x20+@Yend]
-  STR     R5, [SP,#0x20+@Xout]
-  STR     R8, [SP,#0x20+@Yout]
-  STR     R7, [SP,#0x20+@Width]
-  STR     R4, [SP,#0x20+@Height]
+  STR     R6, [SP,@Yend]
+  STR     R5, [SP,@Xout]
+  STR     R8, [SP,@Yout]
+  STR     R7, [SP,@Width]
+  STR     R4, [SP,@Height]
   MOV     R0, #5
-  STR     R0, [SP,#0x20+@Palette]
+  STR     R0, [SP,@Palette]
   LDR     R0, [R10,#0x6C]
   MOV     R3, #29
   MOV     R1, #4
@@ -116,15 +118,15 @@
   BL      @v3d_setSubImage
 
 ; # Small Down Arrow (5/0): [39, 28] -> [49, 40]
-  STR     R9, [SP,#0x20+@Ystart]
+  STR     R9, [SP,@Ystart]
   MOV     R0, #49
   STMFA   SP, {R0,R6}
-  STR     R5, [SP,#0x20+@Xout]
-  STR     R8, [SP,#0x20+@Yout]
-  STR     R7, [SP,#0x20+@Width]
-  STR     R4, [SP,#0x20+@Height]
+  STR     R5, [SP,@Xout]
+  STR     R8, [SP,@Yout]
+  STR     R7, [SP,@Width]
+  STR     R4, [SP,@Height]
   MOV     R0, #1
-  STR     R0, [SP,#0x20+@Palette]
+  STR     R0, [SP,@Palette]
   LDR     R0, [R10,#0x6C]
   MOV     R1, #5
   ADD     R0, R0, #0x128
@@ -133,15 +135,15 @@
   BL      @v3d_setSubImage
 
 ; # Small Down Arrow Other Palette (6/0): [39, 28] -> [49, 40]
-  STR     R9, [SP,#0x20+@Ystart]
+  STR     R9, [SP,@Ystart]
   MOV     R0, #49
   STMFA   SP, {R0,R6}
-  STR     R5, [SP,#0x20+@Xout]
-  STR     R8, [SP,#0x20+@Yout]
-  STR     R7, [SP,#0x20+@Width]
-  STR     R4, [SP,#0x20+@Height]
+  STR     R5, [SP,@Xout]
+  STR     R8, [SP,@Yout]
+  STR     R7, [SP,@Width]
+  STR     R4, [SP,@Height]
   MOV     R0, #2
-  STR     R0, [SP,#0x20+@Palette]
+  STR     R0, [SP,@Palette]
   LDR     R0, [R10,#0x6C]
   MOV     R1, #6
   MOV     R2, #0
@@ -150,15 +152,15 @@
   BL      @v3d_setSubImage
 
 ; # Small Down Arrow Other Palette (7/0): [39, 28] -> [49, 40]
-  STR     R9, [SP,#0x20+@Ystart]
+  STR     R9, [SP,@Ystart]
   MOV     R0, #49
   STMFA   SP, {R0,R6}
-  STR     R5, [SP,#0x20+@Xout]
-  STR     R8, [SP,#0x20+@Yout]
-  STR     R7, [SP,#0x20+@Width]
-  STR     R4, [SP,#0x20+@Height]
+  STR     R5, [SP,@Xout]
+  STR     R8, [SP,@Yout]
+  STR     R7, [SP,@Width]
+  STR     R4, [SP,@Height]
   MOV     R0, #3
-  STR     R0, [SP,#0x20+@Palette]
+  STR     R0, [SP,@Palette]
   LDR     R0, [R10,#0x6C]
   MOV     R1, #7
   MOV     R2, #0
@@ -167,15 +169,15 @@
   BL      @v3d_setSubImage
 
 ; # Small Down Arrow Other Palette (8/0): [39, 28] -> [49, 40]
-  STR     R9, [SP,#0x20+@Ystart]
+  STR     R9, [SP,@Ystart]
   MOV     R0, #49
   STMFA   SP, {R0,R6}
-  STR     R5, [SP,#0x20+@Xout]
-  STR     R8, [SP,#0x20+@Yout]
-  STR     R7, [SP,#0x20+@Width]
-  STR     R4, [SP,#0x20+@Height]
+  STR     R5, [SP,@Xout]
+  STR     R8, [SP,@Yout]
+  STR     R7, [SP,@Width]
+  STR     R4, [SP,@Height]
   MOV     R0, #4
-  STR     R0, [SP,#0x20+@Palette]
+  STR     R0, [SP,@Palette]
   LDR     R0, [R10,#0x6C]
   MOV     R2, #0
   ADD     R0, R0, #0x128
@@ -184,15 +186,15 @@
   BL      @v3d_setSubImage
 
 ; # Small Down Arrow Other Palette (9/0): [39, 28] -> [49, 40]
-  STR     R9, [SP,#0x20+@Ystart]
+  STR     R9, [SP,@Ystart]
   MOV     R0, #49
   STMFA   SP, {R0,R6}
-  STR     R5, [SP,#0x20+@Xout]
-  STR     R8, [SP,#0x20+@Yout]
-  STR     R7, [SP,#0x20+@Width]
-  STR     R4, [SP,#0x20+@Height]
+  STR     R5, [SP,@Xout]
+  STR     R8, [SP,@Yout]
+  STR     R7, [SP,@Width]
+  STR     R4, [SP,@Height]
   MOV     R0, #5
-  STR     R0, [SP,#0x20+@Palette]
+  STR     R0, [SP,@Palette]
   LDR     R0, [R10,#0x6C]
   MOV     R1, #9
   MOV     R2, #0
