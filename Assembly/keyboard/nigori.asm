e76dbd5264f1fc44cd0c7978e3c1b36eff336563
Benito Palacios Sánchez <benito356@gmail.com>
2015-04-06 23:01:29 +0200

    Keyboard: Disable nigori key activation

--- Assembly/keyboard/nigori.asm
+++ Assembly/keyboard/nigori.asm
@@ -17,14 +17,25 @@
 
 .arm
 
-; If somehow a nigori key is pressed, do nothing
+; # If somehow a nigori key is pressed, do nothing
 .org 020C6810h
 .area 4h
   BEQ     020C69F4h                       ; Jump to the end
 .endarea
 
 
-; Make impossible select the nigori keys
+; # Make impossible select the nigori keys
 .org 0x020C6778
   MOVNE   R3, #0        ; Max number of rows in keyboard 1 (originally 6)
   MOVEQ   R3, #0        ; Max number of rows in keyboard 2 (originally 3)
+
+
+; # Disable activation of the nigori keys
+.org 0x020C6A70
+  ; Get the current char
+  MOV     R0, R2  ;,LSL#1           ; No need *2 to get index
+  LDRB    R5, [R1,R0]
+
+.org 0x020C6A80
+  ; It was quitting only if the current char was a space, ok, let's say it's
+  LDMFD SP!, {R3-R11,PC}
