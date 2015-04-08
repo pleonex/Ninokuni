a04e5d8df35911ffac24920d24eda77c0923c5fc
Benito Palacios Sánchez <benito356@gmail.com>
2015-04-08 20:51:40 +0200

    Keyboard: disable select char touching

--- Assembly/keyboard/textbox_touch_movement.asm
+++ Assembly/keyboard/textbox_touch_movement.asm
@@ -1,6 +1,6 @@
 ;;----------------------------------------------------------------------------;;
-;;  Hacks for overlay 14 for arm9
-;;  Copyright 2014 Benito Palacios (aka pleonex)
+;;  Disable change the position of the cursor touching the char.
+;;  Copyright 2015 Benito Palacios (aka pleonex)
 ;;
 ;;  Licensed under the Apache License, Version 2.0 (the "License");
 ;;  you may not use this file except in compliance with the License.
@@ -14,26 +14,12 @@
 ;;  See the License for the specific language governing permissions and
 ;;  limitations under the License.
 ;;----------------------------------------------------------------------------;;
-.nds
-.open overlay9_14.bin, 020C18E0h
 
-.relativeinclude on
-.erroronwarning on
+.arm
 
-.include textbox\script_top.asm
-.include keyboard\keys.asm          ; MUST be first since it's overwritten later
-.include keyboard\write_char.asm
-.include keyboard\nigori.asm
-.include keyboard\delete.asm
-.include keyboard\cursor.asm
-.include keyboard\font_space.asm
-.include keyboard\print_keys.asm
-.include keyboard\set_string.asm
-.include keyboard\monster_search_toUpper.asm
-.include keyboard\textbox_touch_movement.asm
-.include pointers\overlay9_14.asm
-.include textbox\menu_request.asm
-.include password\alphabet.asm
-
-.close
-; EOF ;
+; Since all chars in japanese have the same width, it's easy to know
+; what char are touching. In Spanish and other language, letters like the
+; 'i' has a different width. It's very small (an unknown) feature, so we
+; disable it.
+.org 0x020C6000
+  NOP       ; This was a call to update_cursor for that event.
