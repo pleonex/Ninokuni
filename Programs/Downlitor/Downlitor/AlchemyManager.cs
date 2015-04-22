35d7ab7baa806702dfa4b57e4936ac1a94de0994
Benito Palacios <benito356@gmail.com>
2015-04-22 12:52:20 +0200

    Downliter: Implement recipe structures

---
+++ Programs/Downlitor/Downlitor/AlchemyManager.cs
@@ -0,0 +1,66 @@
+﻿//
+//  AlchemyManager.cs
+//
+//  Author:
+//       Benito Palacios <benito356@gmail.com>
+//
+//  Copyright (c) 2015 Benito Palacios
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+using System;
+
+namespace Downlitor
+{
+	public static class AlchemyManager
+	{
+		private static AlchemyManager instance;
+		private const string ItemName = "AlchemyRecipeData.dat";
+
+		private AlchemyRecipe[] recipes;
+
+		private AlchemyManager()
+		{
+			recipes = new AlchemyRecipe[NumEntries];
+			Read();
+		}
+
+		public static AlchemyManager Instance {
+			get {
+				if (instance == null)
+					instance = new AlchemyManager();
+
+				return instance;
+			}
+		}
+
+		public int NumEntries {
+			get;
+			private set;
+		}
+
+		public AlchemyRecipe GetRecipe(int index)
+		{
+			if (index < 0 || index >= NumEntries)
+				throw new ArgumentException("Invalid index");
+
+			return recipes[index];
+		}
+
+		private void Read()
+		{
+
+		}
+	}
+}
+
---
+++ Programs/Downlitor/Downlitor/AlchemyRecipe.cs
@@ -0,0 +1,57 @@
+﻿//
+//  AlchemyRecipe.cs
+//
+//  Author:
+//       Benito Palacios <benito356@gmail.com>
+//
+//  Copyright (c) 2015 Benito Palacios
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+using System;
+
+namespace Downlitor
+{
+	public struct AlchemyRecipe
+	{
+		public AlchemyRecipe(string name, Ingredient ingredient1, Ingredient ingredient2,
+			Ingredient ingredient3)
+		{
+			Name = name;
+			Ingredient1 = ingredient1;
+			Ingredient2 = ingredient2;
+			Ingredient3 = ingredient3;
+		}
+
+		public string Name {
+			get;
+			private set;
+		}
+
+		public Ingredient Ingredient1 {
+			get;
+			private set;
+		}
+
+		public Ingredient Ingredient2 {
+			get;
+			private set;
+		}
+
+		public Ingredient Ingredient3 {
+			get;
+			private set;
+		}
+	}
+}
+
