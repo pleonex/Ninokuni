d9d08549c72312459f6554c106a063b94b50c597
Benito Palacios Sánchez <benito356@gmail.com>
2015-05-21 23:42:38 +0200

    NinoPatcher: MD5 checks

--- Programs/NinoPatcher/NinoPatcher/Patcher.cs
+++ Programs/NinoPatcher/NinoPatcher/Patcher.cs
@@ -21,46 +21,142 @@
 using System;
 using System.IO;
 using System.Security.Cryptography;
+using Xdelta;
+using System.Reflection;
 
 namespace NinoPatcher
 {
     public class Patcher
     {
         private const string PatchId = "NinoPatcher.Resources.PatchES.xdelta";
-        private const string AntiPiracyId = "NinoPatcher.Resources.AP.xdelta";
-        private const string BannerId = "NinoPatcher.Resources.Banner.xdelta";
-        private static readonly string CleanMd5 = "f0e3027b9e97618732b4f2d4298ad0cf";
+        private const long ExpectedLength = 512 * 1024 * 1024;
+        private static readonly string[] Md5s = {
+            "f0e3027b9e97618732b4f2d4298ad0cf", // ROM
+            "", // ROM + Spanish Patch
+            "", // ROM + Spanish Patch + AP
+            "", // ROM + Spanish Patch + Banner
+            ""  // ROM + Spanish Patch + AP + Banner
+        };
+
+
+        public Patcher(bool antipiracy, bool banner)
+        {
+            AntiPiracy = antipiracy;
+            Banner = banner;
+            Translation = true; // By default true, later will disable if needed
+        }
+
+        public bool Translation {
+            get;
+            private set;
+        }
 
         public bool AntiPiracy {
             get;
-            set;
+            private set;
         }
 
         public bool Banner {
             get;
-            set;
+            private set;
         }
 
-        public bool CheckCleanRom(string cleanRom)
+        public string Input {
+            get;
+            private set;
+        }
+
+        public string Output {
+            get;
+            private set;
+        }
+
+        public bool SetInput(string input)
         {
-            bool isClean = false;
+            bool valid = CheckRomSize(input) ? IsMd5Valid(input) : false;
+            if (valid)
+                Input = input;
+
+            return valid;
+        }
+
+        private bool IsMd5Valid(string rom)
+        {
+            int md5Index = GetMd5Index(rom);
+            UpdateAvailablePatchs(md5Index);
+
+            return md5Index != -1;
+        }
+
+        private int GetMd5Index(string rom)
+        {
+            int md5Index;
             using (FileStream fs = 
-                new FileStream(cleanRom, FileMode.Open, FileAccess.Read, FileShare.Read)) {
+                new FileStream(rom, FileMode.Open, FileAccess.Read, FileShare.Read)) {
 
                 MD5 md5 = MD5.Create();
                 string currentHash = BitConverter.ToString(md5.ComputeHash(fs));
                 currentHash = currentHash.Replace("-", "").ToLower();
 
-                isClean = (currentHash == CleanMd5);
+                md5Index = Array.IndexOf<string>(Md5s, currentHash);
                 md5.Clear();
             }
 
-            return isClean;
+            return md5Index;
         }
 
-        public void Patch(string fileIn, string fileOut)
+        private void UpdateAvailablePatchs(int md5Index)
         {
-            throw new NotImplementedException();
+            // If it's invalid do nothing
+            if (md5Index == -1)
+                return;
+
+            // If it's clean everything is available
+            if (md5Index == 0)
+                return;
+
+            // Other rom types has the translation patch already, so disable it
+            Translation = false;
+
+            // If it has already the AP patch (maybe the user wants the banner too)
+            if (md5Index == 2)
+                AntiPiracy = false;
+
+            // If it has already the banner patch (maybe the user wants the AP too)
+            if (md5Index == 3)
+                Banner = false;
+
+            // If it has everything already (maybe the user is stupid)
+            if (md5Index == 4) {
+                AntiPiracy = false;
+                Banner = false;
+            }
+        }
+
+        private bool CheckRomSize(string cleanRom)
+        {
+            return new FileInfo(cleanRom).Length == ExpectedLength;
+        }
+
+        public bool SetOutput(string output)
+        {
+            Output = output;
+            return true;
+        }
+
+        public void Patch()
+        {
+            if (string.IsNullOrEmpty(Input) || string.IsNullOrEmpty(Output))
+                throw new ArgumentException();
+
+            using (var fsIn  = new FileStream(Input,  FileMode.Open, FileAccess.Read,      FileShare.Read)) {
+            using (var fsOut = new FileStream(Output, FileMode.Open, FileAccess.ReadWrite, FileShare.Read)) {
+                    Stream patch = Assembly.GetExecutingAssembly().GetManifestResourceStream(
+                        PatchId);
+                    Decoder decoder = new Decoder(fsIn, patch, fsOut);
+                    decoder.Run();
+            }
+            }
         }
     }
 }
