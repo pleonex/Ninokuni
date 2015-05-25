3108b1a0ed730eaf76600d7197b8f36a52ba7f06
Benito Palacios Sánchez <benito356@gmail.com>
2015-05-25 22:52:57 +0200

    NinoPatcher: Implement AP patch

--- Programs/NinoPatcher/NinoPatcher/Patcher.cs
+++ Programs/NinoPatcher/NinoPatcher/Patcher.cs
@@ -24,6 +24,7 @@ using System.IO;
 using System.Reflection;
 using System.Security.Cryptography;
 using Xdelta;
+using System.Collections.Generic;
 
 namespace NinoPatcher
 {
@@ -33,6 +34,44 @@ namespace NinoPatcher
     {
         private const string GameTitle = "Ninokuni\nEl Mago de las Tinieblas\nLEVEL5";
         private const string PatchId = "PatchES.xdelta";
+        private static readonly Dictionary<uint, byte[]> ApPatch = new Dictionary<uint, byte[]> {
+            { 0x00004200, new byte[] {
+                    0x00, 0x00, 0x9F, 0xE5, 0x1E, 0xFF, 0x2F, 0xE1,
+                    0xCF, 0xB3, 0x00, 0x00, 0x00, 0x00, 0x9F, 0xE5 } },
+            { 0x00004210, new byte[] {
+                    0x1E, 0xFF, 0x2F, 0xE1, 0x77, 0xB1, 0x00, 0x00,
+                    0x07, 0x40, 0x2D, 0xE9, 0x1C, 0x00, 0x9F, 0xE5 } },
+            { 0x00004220, new byte[] {
+                    0x1C, 0x10, 0x9F, 0xE5, 0x00, 0x20, 0x91, 0xE5,
+                    0x02, 0x00, 0x50, 0xE1, 0x14, 0x00, 0x9F, 0x05 } },
+            { 0x00004230, new byte[] {
+                    0x00, 0x00, 0x81, 0x05, 0x0C, 0x00, 0x80, 0x02,
+                    0x3C, 0x00, 0x81, 0x05, 0x07, 0x80, 0xBD, 0xE8 } },
+            { 0x00004240, new byte[] {
+                    0xE0, 0xD7, 0x15, 0x02, 0x4C, 0xC0, 0x15, 0x02,
+                    0x00, 0x23, 0x00, 0x02 } },
+            { 0x000049F8, new byte[] {
+                    0x06, 0xFE, 0xFF, 0xEA } }
+        };
+        private static readonly Dictionary<uint, byte[]> UnApPatch = new Dictionary<uint, byte[]> {
+            { 0x00004200, new byte[] {
+                    0xE2, 0x26, 0x89, 0x33, 0x28, 0x01, 0x4D, 0x39,
+                    0xBD, 0xDA, 0xEC, 0xC9, 0x7C, 0xCD, 0xAB, 0x7F } },
+            { 0x00004210, new byte[] {
+                    0xDC, 0x75, 0xB3, 0xD1, 0xE8, 0x0B, 0x68, 0x3F,
+                    0x9A, 0xF4, 0x3C, 0x02, 0x0D, 0xD2, 0xAB, 0xDE } },
+            { 0x00004220, new byte[] {
+                    0x41, 0xDD, 0x29, 0x28, 0xFE, 0xC0, 0xB7, 0x19,
+                    0xDC, 0xB6, 0xC6, 0x32, 0x7C, 0x20, 0xCB, 0x0F } },
+            { 0x00004230, new byte[] {
+                    0x7C, 0x6C, 0xFF, 0x7D, 0xEB, 0x06, 0xF6, 0x56,
+                    0x79, 0x79, 0xD6, 0x27, 0x44, 0xEB, 0xCE, 0x5D } },
+            { 0x00004240, new byte[] {
+                    0xCF, 0x1E, 0x36, 0x1B, 0xC6, 0xEF, 0x89, 0x63,
+                    0xBF, 0xFF, 0x8E, 0x8A } },
+            { 0x000049F8, new byte[] {
+                    0x1E, 0xFF, 0x2F, 0xE1 } }
+        };
 
         public Patcher(bool antipiracy, bool banner)
         {
@@ -110,14 +149,18 @@ namespace NinoPatcher
                 using (var outStream =
                     new FileStream(Output, FileMode.Create, FileAccess.ReadWrite, FileShare.Read)) {
                 
-                    if (RemoveAntiPiracy)
-                        UnpatchAntipiracy();
+                    if (RemoveAntiPiracy) {
+                        // In this case it needs write permissions to edit origin...
+                        using (FileStream inStream = 
+                               new FileStream(Input, FileMode.Open, FileAccess.ReadWrite, FileShare.Read))
+                            UnpatchAntipiracy(inStream);
+                    }
 
                     if (Translation)
                         ApplyTranslation(worker, outStream);
 
                     if (AntiPiracy)
-                        ApplyAntipiracy();
+                        ApplyAntipiracy(outStream);
 
                     if (Banner)
                         ApplyBanner(outStream);
@@ -145,6 +188,8 @@ namespace NinoPatcher
             using (FileStream inStream = 
                 new FileStream(Input, FileMode.Open, FileAccess.Read, FileShare.Read)) {
                 using (Stream patch = ResourcesManager.GetStream(PatchId)) {
+                    outStream.Position = 0;
+
                     Decoder decoder = new Decoder(inStream, patch, outStream);
                     decoder.ProgressChanged += delegate (double p) {
                         worker.ReportProgress((int)(p * 100));
@@ -155,14 +200,22 @@ namespace NinoPatcher
             }
         }
 
-        private void ApplyAntipiracy()
+        private void ApplyAntipiracy(Stream outStream)
         {
-            throw new NotImplementedException();
+            ApplyCodePatch(outStream, ApPatch);
         }
 
-        private void UnpatchAntipiracy()
+        private void UnpatchAntipiracy(Stream outStream)
         {
-            throw new NotImplementedException();
+            ApplyCodePatch(outStream, UnApPatch);
+        }
+
+        private void ApplyCodePatch(Stream outStream, Dictionary<uint, byte[]> patch)
+        {
+            foreach (KeyValuePair<uint, byte[]> codePatch in patch) {
+                outStream.Position = codePatch.Key;
+                outStream.Write(codePatch.Value, 0, codePatch.Value.Length);
+            }
         }
 
         private void ApplyBanner(Stream outStream)
