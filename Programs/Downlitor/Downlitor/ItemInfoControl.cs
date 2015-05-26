525d01e0549f46e0f96245808d15bdfe75e3b826
Benito Palacios Sánchez <benito356@gmail.com>
2015-05-26 11:26:38 +0200

    Downlitor: Add item index for multiple items with same name

--- Programs/Downlitor/Downlitor/ItemInfoControl.cs
+++ Programs/Downlitor/Downlitor/ItemInfoControl.cs
@@ -18,7 +18,7 @@ namespace Downlitor
             var manager = ItemManager.Instance;
             for (int i = 0; i < ItemManager.NumEntries; i++) {
                 var item = manager.GetItem(i);
-                comboItems.Items.Add((item == null) ? "Invalid" : item);
+                comboItems.Items.Add((item == null) ? "Invalid" : item + " (" + i.ToString("D3") + ")");
             }
             comboItems.SelectedIndex = dlc.Index;
             comboItems.AutoCompleteMode = AutoCompleteMode.Suggest;
