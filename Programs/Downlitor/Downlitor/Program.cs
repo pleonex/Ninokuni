f5ccacf32f4c6b0d85050f5f27c9f9009eaa6dd7
Benito Palacios Sánchez <benito356@gmail.com>
2015-05-27 13:29:52 +0200

    Downlitor: Export dlc list from encoded bin

--- Programs/Downlitor/Downlitor/Program.cs
+++ Programs/Downlitor/Downlitor/Program.cs
@@ -19,6 +19,7 @@
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 using System;
+using System.IO;
 using System.Xml.Linq;
 using Libgame;
 
@@ -26,6 +27,11 @@ namespace Downlitor
 {
 	public static class Program
 	{
+        private static readonly byte[] Key = new byte[] {
+            0x72, 0x2B, 0x41, 0x8B, 0x4C, 0xFB, 0x9F, 0x27,
+            0xB2, 0x1D, 0x05, 0xAF, 0xFB, 0x2B, 0x80, 0x9F
+        };
+
 		[STAThread]
 		public static void Main(string[] args)
 		{
@@ -33,17 +39,20 @@ namespace Downlitor
 			var document = XDocument.Load(xmlEdit);
 			Configuration.Initialize(document);
 
-            if (args.Length == 2 && args[0] == "-e") {
-                Export(args[1]);
+            if (args.Length >= 2 && args[0] == "-e") {
+                if (args.Length == 3)
+                    ExportFromDlc(args[1], args[2]);
+                else
+                    Export(args[1]);
                 return;
-            }
+            } 
 
             MainWindow window = new MainWindow();
             window.ShowDialog();
             window.Dispose();
 		}
 
-        private static void Export(string output)
+        private static void Export(string output, bool[] activation = null)
         {
             XDocument xml = new XDocument();
             XElement root = new XElement("NinoDlc");
@@ -53,12 +62,43 @@ namespace Downlitor
                 XElement entry = new XElement("Element");
                 entry.SetAttributeValue("ID", i.ToString("D2"));
                 entry.SetAttributeValue("Name", manager[i]);
-                entry.Value = "false";
+                entry.Value = (activation == null) ? "false" : activation[i].ToString();
                 root.Add(entry);
             }
 
             xml.Add(root);
             xml.Save(output);
+        }
+
+        private static void ExportFromDlc(string output, string dlc)
+        {
+            var activation = GetActivation(dlc);
+            Export(output, activation);
+        }
+
+        private static bool[] GetActivation(string dlc)
+        {
+            bool[] activation = new bool[0x72];
+            var dlcStream = new FileStream(dlc, FileMode.Open);
+            var dlcDecoded = Rc4.Run(dlcStream, Key);
+            dlcStream.Dispose();
+
+            dlcDecoded.Position = 8;
+            byte bitCount = 0;
+            byte value = 0;
+            for (int i = 0; i < activation.Length; i++) {
+                if (bitCount == 0) {
+                    value = (byte)dlcDecoded.ReadByte();
+                    bitCount = 8;
+                }
+
+                activation[i] = (value & 1) == 1;
+                value >>= 1;
+                bitCount--;
+            }
+
+            dlcDecoded.Dispose();
+            return activation;
         }
 	}
 }
---
+++ Programs/Downlitor/Downlitor/Rc4.cs
@@ -0,0 +1,70 @@
+﻿//
+//  Program.cs
+//
+//  Author:
+//       Benito Palacios Sánchez <benito356@gmail.com>
+//
+//  Copyright (c) 2015 Benito Palacios Sánchez
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+using System;
+using System.IO;
+
+namespace Downlitor
+{
+    public static class Rc4
+    {
+        public static Stream Run(Stream data, byte[] key)
+        {
+            byte[] s = CreateArrayS(key);
+            Stream output = new MemoryStream((int)data.Length);
+
+            for (int i = 0, j = 0; data.Position < data.Length; ) {
+                i = (i + 1) % 256;
+                j = (j + s[i]) % 256;
+
+                byte swap = s[i];
+                s[i] = s[j];
+                s[j] = swap;
+
+                byte k = s[(s[i] + s[j]) % 256];
+                output.WriteByte((byte)(data.ReadByte() ^ k));
+            }
+
+            return output;
+        }
+
+        private static byte[] CreateArrayS(byte[] key)
+        {
+            byte[] s = new byte[256];
+            byte[] t = new byte[256];
+
+            for (int i = 0; i < 256; i++) {
+                s[i] = (byte)i;
+                t[i] = key[i % key.Length];
+            }
+                
+            for (int i = 0, j = 0; i < 256; i++) {
+                j = (j + s[i] + t[i]) % 256;
+
+                byte swap = s[i];
+                s[i] = s[j];
+                s[j] = swap;
+            }
+
+            return s;
+        }
+    }
+}
+
