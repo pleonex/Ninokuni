44ef64d1db285f2b20c0874c73506f7dd52c9ca1
Benito Palacios Sánchez <benito356@gmail.com>
2015-06-03 22:58:44 +0200

    DlcUpdater: Read XML for information

--- Programs/scripts/DlcUpdater.py
+++ Programs/scripts/DlcUpdater.py
@@ -17,10 +17,11 @@
 #  limitations under the License.                                             #
 ###############################################################################
 
-# from argparse import ArgumentParser
+from argparse import ArgumentParser
 # from struct import unpack
-from os import listdir
+from os import listdir, path
 from datetime import date, datetime
+import xml.etree.ElementTree as ET
 
 
 def should_update_file(path, prefix):
@@ -37,5 +38,46 @@ def should_update_file(path, prefix):
     return file_date < date.today()
 
 
+def update_distribution(node, base_path, lang):
+    # Get output path and date format
+    out_path = path.join(base_path, node.get("output"))
+    date_format = node.get("dateFormat")
+
+    # Get distribution node
+    if not should_update_file(out_path, lang + "_" + "tweet"):
+        print "Distribution already updated"
+        return
+    
+    print date_format
+
+
+def update_magic_news(node, base_path, lang):
+    # Not implemented
+    pass
+
+
+def update_translation_files(node, base_path):
+    lang = node.get("language")
+
+    # Get distribution node
+    distribution = node.find("Distribution")
+    update_distribution(distribution, base_path, lang)
+
+    # Get magic news node
+    magic_news = node.find("MagicNews")
+    update_magic_news(magic_news, base_path, lang)
+
+
 if __name__ == "__main__":
-    print(should_update_file(".", "tweet"))
+    # Get arguments
+    parser = ArgumentParser(description="Update DLC files of Ninokuni DS")
+    parser.add_argument('dlcinfo', help='path to XML with the info')
+    args = parser.parse_args()
+
+    # Get base path (the one from the xml)
+    base_path = path.dirname(path.realpath(args.dlcinfo))
+
+    # Get translations nodes
+    root = ET.parse(args.dlcinfo).getroot()
+    for trans in root.findall("Ninokuni"):
+        update_translation_files(trans, base_path)
