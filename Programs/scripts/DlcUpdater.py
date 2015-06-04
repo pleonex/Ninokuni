38cdce507f069781077cb2ada13b8c4b9993fb0b
Benito Palacios Sánchez <benito356@gmail.com>
2015-06-04 02:07:27 +0200

    DlcUpdater: Implement RC4 and write to file

--- Programs/scripts/DlcUpdater.py
+++ Programs/scripts/DlcUpdater.py
@@ -20,13 +20,55 @@
 from argparse import ArgumentParser
 from struct import pack
 from os import listdir, path
-from datetime import datetime
+from datetime import date, datetime
 from binascii import crc32
 import xml.etree.ElementTree as ET
 
 # Set today as global variable just in case the day change while the
 # program is running (very very rare case but...)
-TODAY = datetime.today()
+TODAY = date.today()
+
+# Date format for filenames
+FILE_DATE_FORMAT = "%m%d%Y"
+
+
+def rc4(data):
+    KEY = bytearray([0x72, 0x2B, 0x41, 0x8B, 0x4C, 0xFB, 0x9F, 0x27,
+                    0xB2, 0x1D, 0x05, 0xAF, 0xFB, 0x2B, 0x80, 0x9F])
+
+    i = j = 0
+    s = rc4_create_array_s(KEY)
+    for index in range(len(data)):
+        i = (i + 1) % 256
+        j = (j + s[i]) % 256
+
+        swap = s[i]
+        s[i] = s[j]
+        s[j] = swap
+
+        k = s[(s[i] + s[j]) % 256]
+        data[index] ^= k
+
+    return data
+
+
+def rc4_create_array_s(key):
+    s = bytearray(256)
+    t = bytearray(256)
+
+    for i in range(256):
+        s[i] = i
+        t[i] = key[i % len(key)]
+
+    j = 0
+    for i in range(256):
+        j = (j + s[i] + t[i]) % 256
+
+        swap = s[i]
+        s[i] = s[j]
+        s[j] = swap
+
+    return s
 
 
 def add_distribution_header(data):
@@ -40,21 +82,19 @@ def xml2binary(node, date_format):
     INVALID_DATE = datetime(1, 1, 1)
 
     data = bytearray(NUM_ELEMENTS)
-    for element in node.findall("Element"):
-        element_date = datetime.strptime(element.get("releaseOn"), date_format)
+    for el in node.findall("Element"):
+        el_date = datetime.strptime(el.get("releaseOn"), date_format).date()
 
-        if element_date != INVALID_DATE and element_date <= TODAY:
-            element_id = int(element.get("id"))
-            byte_index = element_id / 8
-            bit_index = element_id % 8
+        if el_date != INVALID_DATE and el_date <= TODAY:
+            el_id = int(el.get("id"))
+            byte_index = el_id / 8
+            bit_index = el_id % 8
             data[byte_index] |= (1 << bit_index)
 
     return data
 
 
 def should_update_file(path, prefix):
-    FILE_DATE_FORMAT = "%m%d%Y"
-
     # Get full name without extension and basename
     date_string = ""
     for file in listdir(path):
@@ -76,11 +116,17 @@ def update_distribution(node, base_path, lang):
     date_format = node.get("dateFormat")
 
     # Get distribution node
-    if should_update_file(out_path, lang + "_" + "tweet"):
+    prefix = lang + "_tweet"
+    if should_update_file(out_path, prefix):
         print "Updating distribution DLC"
         data = xml2binary(node, date_format)
         data = add_distribution_header(data)
-        print '-'.join('{:02x}'.format(x) for x in data)
+        data = rc4(data)
+
+        # Write to file
+        fname = path.join(out_path, prefix + TODAY.strftime(FILE_DATE_FORMAT))
+        with open(fname + ".bin", "wb") as file:
+            file.write(data)
 
 
 def update_magic_news(node, base_path, lang):
