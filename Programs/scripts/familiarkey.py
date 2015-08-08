37b2ed35c025aa880492399bc88ada8d5db220f5
Benito Palacios Sánchez <benito356@gmail.com>
2015-08-08 15:24:13 +0200

    Implement alphabet 2 to 1 in familiar key

--- Programs/scripts/familiarkey.py
+++ Programs/scripts/familiarkey.py
@@ -21,7 +21,8 @@ limitations under the License.
 from argparse import ArgumentParser
 
 ALPHABET1 = "0123456789abcdefghijkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ"
-ALPHABET2_ORIGINAL = "０１２３４５６７８９"
+ALPHABET2 = ""  # Selected in run-time.
+ALPHABET2_JAPANESE = "０１２３４５６７８９"
 "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをん！？"
 ALPHABET2_SPANISH = "!#$%&()*+,-./0123456789:;<=>?@"
 "ABCDEFGHJKLMNPQRSTUVWXYZ[\\]_abcdefghijkmnpqrstuvwxyz{|}"
@@ -49,13 +50,23 @@ def text_to_key(text):
 ####################################
 def get_familiar_info(text_key):
     """Get a dictionary with familiar information from the key."""
+    text_key = alphabet2_to_alphabet1(text_key)
     key = key_to_text(text_key)
 
     return None
 
 
+def alphabet2_to_alphabet1(text_key):
+    """1.- Convert the string from alphabet2 representation to alphabet1."""
+    for i in range(len(text_key)):
+        idx = ALPHABET2.find(text_key[i])
+        text_key[i] = ALPHABET1[idx]
+
+    return text_key
+
+
 def key_to_text(key):
-    """Convert the key bytes into text representation."""
+    """2.- Convert the key bytes into text representation."""
     text = ''
     while key != 0:
         index = key % 0x3A
@@ -69,7 +80,17 @@ if __name__ == "__main__":
     # Parse argument
     parser = ArgumentParser(description="Get familiar information from a key.")
     parser.add_argument("key", help="The familiar key")
+    parser.add_argument("--language",
+                        help="The game language where the key was generated.",
+                        choices=['japanese', 'spanish'],
+                        default='japanese')
     args = parser.parse_args()
+
+    # Select the alphabet2 to use depending the game version.
+    if args.language == "japanese":
+        ALPHABET2 = ALPHABET2_JAPANESE
+    elif args.language == "spanish":
+        ALPHABET2 = ALPHABET2_SPANISH
 
     # Decrypt key
     info = get_familiar_info(args.key)
