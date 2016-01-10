e6b0123ded41b639f820016533721ccab3a3d52b
Benito Palacios Sánchez <benito356@gmail.com>
2016-01-10 20:57:44 +0100

    Create patch too while compiling

--- Compiler/compile_linux.sh
+++ Compiler/compile_linux.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+
+# Generate game
 mono modime.exe --set-input-names=Ninokuni.nds -i "Ninokuni.xml" "Ninokuni english.xml" "$1" "$HOME/Ninokuni [PATCHED].nds"
+
+# Generate patch
+echo "Making patch..."
+xdelta -e -9 -s "$1" "$HOME/Ninokuni [PATCHED].nds" "$HOME/Ninokuni.patch"
