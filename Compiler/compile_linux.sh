3ace25cbfb457da6023f9b8ddeaefc8036c08a13
Benito Palacios Sánchez <benito356@gmail.com>
2016-02-25 02:02:04 +0100

    Generate patch with image changes

--- Compiler/compile_linux.sh
+++ Compiler/compile_linux.sh
@@ -4,10 +4,23 @@ SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 OUTPUT_ROM="${SCRIPT_DIR}/../GameData/Ninokuni [PATCHED].nds"
 OUTPUT_PATCH="${SCRIPT_DIR}/../GameData/Ninokuni.xdelta"
 
+ORIGINAL_ROM=$1
+TRANSLATION_DIR=$2
+
+# Generate the images
+mono "${SCRIPT_DIR}/linux/ninoimager.exe" -i \
+    "${TRANSLATION_DIR}/Images/Translated/"  \
+    "${TRANSLATION_DIR}/Images/N2D/"         \
+    "${SCRIPT_DIR}/Ninokuni.xml"             \
+    "${SCRIPT_DIR}/Ninokuni english.xml"     \
+    "${SCRIPT_DIR}/linux/MultiImport.xml"
+
 # Generate game
-mono modime.exe --set-input-names=Ninokuni.nds -i "${SCRIPT_DIR}/Ninokuni.xml" "${SCRIPT_DIR}/Ninokuni english.xml" \
+mono "${SCRIPT_DIR}/modime.exe" --set-input-names=Ninokuni.nds -i \
+    "${SCRIPT_DIR}/Ninokuni.xml"         \
+    "${SCRIPT_DIR}/Ninokuni english.xml" \
     "$1" "${OUTPUT_ROM}"
 
 # Generate patch
 echo "Making patch..."
-xdelta -e -9 -f -s "$1" "${OUTPUT_ROM}" "${OUTPUT_PATCH}"
+xdelta -e -9 -f -s "${ORIGINAL_ROM}" "${OUTPUT_ROM}" "${OUTPUT_PATCH}"
